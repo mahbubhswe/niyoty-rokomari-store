0dab811d9681bdeb0d5366c49d1cfe5ab98dcc48
mahbubhswe <99654070+mahbubhswe@users.noreply.github.com>
2022-02-17 21:08:39 +0600

    Update index.js

--- pages/dashboard/cost-record/index.js
+++ pages/dashboard/cost-record/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import DashboardLayout from "../../../components/DashboardLayout";
 import { db } from "../../../firebase";
 
-import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 
 export default function Index() {
   const [costRecord, setCostRecord] = useState([]);
@@ -17,28 +17,27 @@ export default function Index() {
   }, []);
   return (
     <DashboardLayout>
-      <h1 className="text-center">সকল খরচ</h1>
       <div className="container">
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th scope="col">খরচের পরিমাণ</th>
-              <th scope="col">বাবদ</th>
-              <th scope="col">বিবরণ</th>
-              <th scope="col">তারিখ</th>
-            </tr>
-          </thead>
-          <tbody>
-            {costRecord.map((item) => (
-              <tr key={item.id}>
-                <td>{item.cost}</td>
-                <td>{item.category}</td>
-                <td>{item.description}</td>
-                <td>{item.date}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <h1>সকল খরচ</h1>
+        {costRecord
+          ? costRecord.map((x) => (
+              <div key={x.id}>
+                <div className="alert alert-success text-black">
+                  <strong>
+                    {" "}
+                    {"খরচের পরিমাণ: " +
+                      x.cost +
+                      "        বাবদ: " +
+                      x.category +
+                      "        বিস্তারিত: " +
+                      x.description +
+                      "        তারিখ: " +
+                      x.date}
+                  </strong>
+                </div>
+              </div>
+            ))
+          : null}
       </div>
     </DashboardLayout>
   );
